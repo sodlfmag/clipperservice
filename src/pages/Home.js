caa13eab01889867a6c3a8dc9110c3ee52097544
sodlfmag <sodlfmag@khu.ac.kr>
2023-03-17 18:38:16 +0900

    FEAT: 비주얼라이저 영상 삽입

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,20 @@
+// video를 muted 한 상태에서만 자동재생이 가능.
+// 소리를 켜야하는 경우는 추가 옵션 필요
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <div className="VideoContainer">
+        <video className="VisualizerVideo" loop autoplay="" muted>
+          <source
+            src={process.env.PUBLIC_URL + `/assets/SampleVisualizerVideo.mp4`}
+            type="video/mp4"
+          />
+        </video>
+      </div>
+    </>
+  );
 };
 
 export default Home;
