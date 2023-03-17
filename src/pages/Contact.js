a5373dfce45163ab8e7a74f1c1590e9eb08c11a7
sodlfmag <sodlfmag@khu.ac.kr>
2023-03-18 05:35:12 +0900

    FEAT: Contact 페이지 추가

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,34 @@
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div className="BusinessCard">
+        <div className="LogoContainer">
+          <img
+            className="Logo"
+            width="254px"
+            src={process.env.PUBLIC_URL + `/assets/BlackLogo.png`}
+          />
+        </div>
+        <div className="CorpInfoContainer">
+          <div className="CorpInfo">
+            ADDRESS: 서울특별시 관악구 남부순환로 161가 길 16
+          </div>
+          <div className="CorpInfo">
+            BUSINESS MAIL: clipper.co.service@gmail.com
+          </div>
+          <div className="CorpInfo">MOBILE: +82 10-9597-1090 (디자인실)</div>
+          <div className="CorpInfo">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            +82 10-2262-6091(기획실)
+          </div>
+          <div className="CorpInfo">FAX: 0504-494-1090</div>
+        </div>
+      </div>
+      <div className="RequestButtonContainer">
+        <button className="RequestButton">협업 문의하기</button>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
