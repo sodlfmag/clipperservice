65bb73624964931e8c76f4b1bdb4362f4f7b1e5e
sodlfmag <sodlfmag@khu.ac.kr>
2023-03-18 04:20:27 +0900

    FEAT: 포트폴리오 요소가 한 줄을 채우면 아래쪽으로 내려가도록 wrapper 구현

---
+++ src/components/SamplePortfolio.js
@@ -0,0 +1,80 @@
+// const element = {
+//   src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+//   name: "Owen - Cry",
+//   agency: "Eclipse Empire",
+//   date: "2022-10-31",
+// };
+const elements = [
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+  {
+    src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+    name: "Owen - Cry",
+    agency: "Eclipse Empire",
+    date: "2022-10-31",
+  },
+];
+
+const getPortfolios = () => {
+  return elements;
+};
+
+export default getPortfolios;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,26 +2,24 @@ import React from "react";
 
 import PageBody from "../components/PageBody";
 import PortfolioElement from "../components/PortfolioElement";
-import PageTitle from "../components/PageTitle";
+import getPortfolios from "../components/SamplePortfolio";
 
 //[점검] 포트폴리오 이미지가 이미 round되고 그림자가 있는데 해당 효과가 css로 필요한지
 
-const element = {
-  src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
-  name: "Owen - Cry",
-  agency: "Eclipse Empire",
-  date: "2022-10-31",
-};
-const Portfolio = ({ name, introduction }) => {
+const elements = getPortfolios();
+const Portfolio = () => {
   return (
     <>
       <PageBody
         name="PORTFOLIO"
         introduction="클리퍼와 함께한 작업들 입니다."
       />
-      <div className="PageBody">
-        <PageBody name={name} introduction={introduction} />
-        <PortfolioElement element={element} />
+      <div className="PortfolioList">
+        {elements.map((element) => (
+          <>
+            <PortfolioElement element={element} />
+          </>
+        ))}
       </div>
     </>
   );
