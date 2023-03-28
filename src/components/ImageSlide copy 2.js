5d54748b01a1751e60b492f2feaed67c84e8c225
sodlfmag <sodlfmag@khu.ac.kr>
2023-03-28 13:10:26 +0900

    FIX: 갤러리 4개 슬라이드 dot 삭제

--- src/components/ImageSlide copy 2.js
+++ src/components/ImageSlide copy 2.js
@@ -74,7 +74,7 @@ const ImageSlide4 = () => {
           slidesToSlide: 1,
         },
       }}
-      showDots
+      showDots={false}
       sliderClass=""
       slidesToSlide={1}
       swipeable
@@ -126,76 +126,5 @@ const ImageSlide4 = () => {
     </Carousel>
   );
 };
-
-// const ImageSlide4 = () => {
-//   return (
-//     <div className="carousel-wrapper">
-//       <Carousel
-//         style={{ width: "80%" }}
-//         showDots={false}
-//         additionalTransfrom={0}
-//         arrows
-//         autoPlaySpeed={3000}
-//         centerMode={false}
-//         className=""
-//         containerClass="container"
-//         dotListClass=""
-//         draggable={false}
-//         focusOnSelect={false}
-//         infinite
-//         itemClass=""
-//         keyBoardControl
-//         minimumTouchDrag={80}
-//         renderButtonGroupOutside={false}
-//         renderDotsOutside={false}
-//         responsive={{
-//           desktop: {
-//             breakpoint: {
-//               max: 3000,
-//               min: 1024,
-//             },
-//             items: 4,
-//             itemWidth: 200,
-//           },
-//           mobile: {
-//             breakpoint: {
-//               max: 464,
-//               min: 0,
-//             },
-//             items: 1,
-//           },
-//           tablet: {
-//             breakpoint: {
-//               max: 1024,
-//               min: 464,
-//             },
-//             items: 2,
-//           },
-//         }}
-//         itemWidth={1000}
-//         sliderClass=""
-//         slidesToSlide={1}
-//         swipeable
-//       >
-//         {portfolioItems.map((item) => (
-//           <div key={item.title}>
-//             <div className="GalleryElement">
-//               <img
-//                 className="portfolio-slide__image"
-//                 src={item.image}
-//                 alt={item.title}
-//                 style={{ borderRadius: "5px" }}
-//               />
-//               <div className="GallerySub">
-//                 <div>{item.title}</div>
-//                 <div>{item.date}</div>
-//               </div>
-//             </div>
-//           </div>
-//         ))}
-//       </Carousel>
-//     </div>
-//   );
-// };
 
 export default ImageSlide4;
