25048ae9f64ac175658b749cccdd8f5d3dd2806c
sodlfmag <sodlfmag@khu.ac.kr>
2023-04-11 17:28:42 +0900

    STYLE: 주석 삭제 및 에디터 title 수정

--- src/pages/AdminEditor.js
+++ src/pages/AdminEditor.js
@@ -2,35 +2,11 @@ import React, { Component } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-// import Image from '@ckeditor/ckeditor5-image/src/image';
-// import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-// import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-// import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-// import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
-// import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage';
-
-// ClassicEditor
-//     .create( document.querySelector( '#editor' ), {
-//         plugins: [ Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage ],
-//         image: {
-//             toolbar: [
-//                 'imageStyle:block',
-//                 'imageStyle:side',
-//                 '|',
-//                 'toggleImageCaption',
-//                 'imageTextAlternative',
-//                 '|',
-//                 'linkImage'
-//             ]
-//         }
-//     } )
-//     .then( /* ... */ )
-//     .catch( /* ... */ );
 class AdminEditor extends Component {
   render() {
     return (
       <div className="AdminEditor">
-        <h2>Using CKEditor 5 build in React</h2>
+        <h2>관리자 모드 에디터</h2>
         <CKEditor
           editor={ClassicEditor}
           data="<p>Hello from CKEditor 5!</p>"
@@ -49,6 +25,7 @@ class AdminEditor extends Component {
             console.log("Focus.", editor);
           }}
         />
+        {/* <button>전송</button> */}
       </div>
     );
   }
