d7832a89ec8efce1f22a5ec0a16201d344a4cf91
sodlfmag <sodlfmag@khu.ac.kr>
2023-04-13 15:44:53 +0900

    FEAT: 포트폴리오 각 요소 클릭 시 각 id 별 포트폴리오 상세페이지 이동 라우팅

--- src/components/PortfolioElement.js
+++ src/components/PortfolioElement.js
@@ -1,18 +1,24 @@
 //[점검] 비구조화 할당, 스프레드 문법으로 prop 받아오는 방법
+
+import { Link } from "react-router-dom";
+
 const PortfolioElement = (props) => {
+  const location = "/portfolio/" + props.element.portfolioId;
   return (
-    <div className="PortfolioElement">
-      <img
-        className="PortfolioImage"
-        src={props.element.clipperImageDtos[0].fileName}
-        width="330px"
-      />
-      <div className="PortfolioSub">
-        <div>{props.element.agency}</div>
-        <div>{props.element.date}</div>
+    <Link to={location}>
+      <div className="PortfolioElement">
+        <img
+          className="PortfolioImage"
+          src={props.element.clipperImageDtos[0].fileName}
+          width="330px"
+        />
+        <div className="PortfolioSub">
+          <div>{props.element.agency}</div>
+          <div>{props.element.date}</div>
+        </div>
+        <div className="PortfolioName">{props.element.title}</div>
       </div>
-      <div className="PortfolioName">{props.element.title}</div>
-    </div>
+    </Link>
   );
 };
 
