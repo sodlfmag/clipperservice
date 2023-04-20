9dc9fe9d02648188876ffd12388124ba2078397a
sodlfmag <sodlfmag@khu.ac.kr>
2023-04-20 18:14:14 +0900

    STYLE: 포트폴리오 샘플  introduction 에디터 nested HTML elements로 교체

--- src/components/SamplePortfolio.js
+++ src/components/SamplePortfolio.js
@@ -53,18 +53,7 @@ const elements = [
     portfolioId: 2,
     title: "BIG Naughty - Bucket List [EP]",
     agency: "H1GERMUSIC’",
-    introduction: `2월 25일 발매된 BIG Naughty 님의 [Buket List]의
-    앨범 커버 디자인과, 피지컬 앨범 디자인을 진행 하였으며
-    앨범 커버에 사용된 소품을 제작하였습니다.
-    
-    해당 소품과 아트워크는 성수 미술관과 하이어뮤직의 협업으로
-    2월 24일부터 약 2개월 간 전시를 진행하였습니다.
-    
-    
-    Design & Production
-    Artwork Design
-    Physical Album Design
-    Art Prop Production
+    introduction: `<p><br>2월 25일 발매된 BIG Naughty 님의 [Buket List]의<br>앨범 커버 디자인과, 피지컬 앨범 디자인을 진행 하였으며<br>앨범 커버에 사용된 소품을 제작하였습니다.<br><br>해당 소품과 아트워크는 성수 미술관과 하이어뮤직의 협업으로<br>2월 24일부터 약 2개월 간 전시를 진행하였습니다.</p><p>&nbsp;</p><p>&nbsp;</p><p>Design &amp; Production</p><ul><li>Artwork Design</li><li>Physical Album Design</li><li>Art Prop Production</li></ul>
     `,
     contents: "대충 내용",
     date: "2021-02-25",
@@ -123,17 +112,7 @@ const elements = [
     portfolioId: 3,
     title: "나선 - Underdogma",
     agency: "PlusN Ent",
-    introduction: ` 8월 24일 발매된 나선님의 Underdogma[EP] 의
-    디지털 앨범 커버의 디자인과, 앨범 커버에 사용된 소품을 제작했습니다.
-    
-    
-    
-    
-    
-    
-    Design & Production
-    Artwork Design
-    Art Prop
+    introduction: `<p>8월 24일 발매된 나선님의 Underdogma[EP] 의&nbsp;<br>디지털 앨범 커버의 디자인과, 앨범 커버에 사용된 소품을 제작했습니다.<br><br><br><br><br><br>&nbsp;</p>Design &amp; Production<ul><li>Artwork Design</li><li>Art Prop</li></ul>
     `,
     contents: "대충 내용",
     date: "2021-08-24",
@@ -180,18 +159,7 @@ const elements = [
     portfolioId: 4,
     title: "BIG Naughty - STAB (Feat. eaJ) (Prod. dress)",
     agency: "H1GERMUSIC",
-    introduction: `11월 18일 발매된 BIG Naughty님의 [STAB] 의
-    디지털 앨범 커버의 디자인과 앨범 커버에 사용된 소품을 제작했습니다.
-    앨범 커버에 사용된 소품은 곡의 비쥬얼 라이저 영상에도 활용되었습니다.
-    
-    
-    
-    
-    
-    Design & Production
-    Artwork Design
-    Art Prop Production
-    Visualizer video
+    introduction: `<p>11월 18일 발매된 BIG Naughty님의 [STAB] 의&nbsp;<br>디지털 앨범 커버의 디자인과 앨범 커버에 사용된 소품을 제작했습니다.<br>앨범 커버에 사용된 소품은 곡의 비쥬얼 라이저 영상에도 활용되었습니다.<br><br>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>Design &amp; Production</p><ul><li>Artwork Design</li><li>Art Prop Production</li><li>Visualizer video</li></ul>
     `,
     contents: "대충 내용",
     date: "2022-12-20",
@@ -256,21 +224,7 @@ const elements = [
     portfolioId: 5,
     title: "Owen - Cry",
     agency: "Eclipse Empire’",
-    introduction: `10월 31일 발매된 Owen님의 정규 앨범 [CRY] 의
-    디지털 앨범 커버와, 타이틀 곡 Jungle 에 사용된
-    뮤직비디오/공연용 헤드피스 소품을 제작했습니다.
-    
-    헤드피스의 모델링 파일을 활용 하여 뮤지션의 라이브 방송과
-    팬들이 사용할 수 있는 인스타그램 AR 필터를 제작했습니다.
-    
-    
-    
-    
-    
-    
-    Design & Production
-    Art Prop (Headpiece)
-    Instagram AR Filtter
+    introduction: `<p>10월 31일 발매된 Owen님의 정규 앨범 [CRY] 의<br>디지털 앨범 커버와, 타이틀 곡 Jungle 에 사용된&nbsp;<br>뮤직비디오/공연용 헤드피스 소품을 제작했습니다.&nbsp;<br><br>헤드피스의 모델링 파일을 활용 하여 뮤지션의 라이브 방송과<br>팬들이 사용할 수 있는 인스타그램 AR 필터를 제작했습니다.<br>&nbsp;</p><p>&nbsp;</p><p><br><br>&nbsp;</p><p>&nbsp;</p><p>Design &amp; Production</p><ul><li>Art Prop (Headpiece)</li><li>Instagram AR Filtter</li></ul>
     `,
     contents: "대충 내용",
     date: "2022-10-31",
@@ -326,20 +280,7 @@ const elements = [
     portfolioId: 6,
     title: "Marv - MAMA [pre-release]",
     agency: "Eclipse Empire",
-    introduction: `12월 20일 발매된 Marv님의  [MAMA]
-    디지털 앨범 커버의 디자인과, Marv님의 음악적 정체성을 담은
-    뮤직비디오/촬영용 헤드피스 소품을 제작했습니다.
-    
-    헤드피스의 모델링 파일을 활용 하여 뮤지션의 라이브 방송과
-    팬들이 사용할 수 있는 인스타그램 AR 필터를 제작했습니다.
-    
-    
-    
-    
-    Design & Production
-    Artwork Design
-    Art Prop (Headpiece)
-    Instagram AR Filtter
+    introduction: `<p>12월 20일 발매된 Marv님의 [MAMA]&nbsp;<br>디지털 앨범 커버의 디자인과, Marv님의 음악적 정체성을 담은&nbsp;<br>뮤직비디오/촬영용 헤드피스 소품을 제작했습니다.&nbsp;<br><br>헤드피스의 모델링 파일을 활용 하여 뮤지션의 라이브 방송과&nbsp;<br>팬들이 사용할 수 있는 인스타그램 AR 필터를 제작했습니다.<br><br><br><br><br><br>&nbsp;</p><p>Design &amp; Production</p><ul><li>Artwork Design</li><li>Art Prop (Headpiece)</li><li>Instagram AR Filtter</li></ul>
     `,
     contents: "대충 내용",
     date: "2022-12-20",
@@ -380,20 +321,7 @@ const elements = [
     portfolioId: 7,
     title: "Khundipanda - The Spoild child 1st Aniversary MD",
     agency: "Dejavu Group",
-    introduction: `Khundipanda 님의 정규 앨범 [The Spoild Child:균] 의 발매 1주년을
-    기념하여 머천다이즈 3종을(뱃지, 쿠션, 후드) 디자인 및 생산했습니다.
-    
-    해당 머천다이즈 중 2종 (뱃지, 쿠션)은 데자부 스토어에서 선 공개되어 판매 되었고
-    후드는 1월 17일에 왓차홀에서 진행 되었던 단독 콘서트에서 판매 되었습니다.
-    
-    머천다이즈의 디자인과 생산 뿐만 아니라 해당 단독 콘서트에서
-    사용된 무대 소품또한 제작 하였습니다.
-    
-    
-    
-    Design & Production
-    MD Production
-    Stage Props Production
+    introduction: `<p>Khundipanda 님의 정규 앨범 [The Spoild Child:균] 의 발매 1주년을<br>기념하여 머천다이즈 3종을(뱃지, 쿠션, 후드) 디자인 및 생산했습니다.<br><br>해당 머천다이즈 중 2종 (뱃지, 쿠션)은 데자부 스토어에서 선 공개되어 판매 되었고<br>후드는 1월 17일에 왓차홀에서 진행 되었던 단독 콘서트에서 판매 되었습니다.<br><br>머천다이즈의 디자인과 생산 뿐만 아니라 해당 단독 콘서트에서&nbsp;<br>사용된 무대 소품또한 제작 하였습니다.<br><br>&nbsp;</p><p>&nbsp;</p><p>Design &amp; Production</p><ul><li>MD Production</li><li>Stage Props Production</li></ul>
     `,
     contents: "대충 내용",
     date: "2023-01-17",
@@ -465,22 +393,7 @@ const elements = [
     portfolioId: 8,
     title: "Marv - Souly [EP]",
     agency: "Eclipse Empire",
-    introduction: `1월 20일 발매된 Marv님의 EP 앨범 [SOULY] 의
-    앨범 전반적인 아트 디렉팅과 디지털 앨범 커버 디자인을 맡았습니다.
-    
-    또한 Marv님의 음악적 정체성을 담은 뮤직비디오/촬영용 헤드피스 소품을
-    제작했습니다. 헤드피스 소품은 뮤직 비디오 뿐만 아니라, 공연이나
-    인터뷰 촬영시에도 사용되었습니다.
-    
-    헤드피스의 모델링 파일을 활용 하여 뮤지션의 라이브 방송과
-    팬들이 사용할 수 있는 인스타그램 AR 필터를 제작했습니다.
-    
-    
-    Design & Production
-    Art Direction
-    Artwork Design
-    Art Prop Production
-    Instagram AR Filtter
+    introduction: `<p>1월 20일 발매된 Marv님의 EP 앨범 [SOULY] 의&nbsp;<br>앨범 전반적인 아트 디렉팅과 디지털 앨범 커버 디자인을 맡았습니다.<br><br>또한 Marv님의 음악적 정체성을 담은 뮤직비디오/촬영용 헤드피스 소품을&nbsp;<br>제작했습니다. 헤드피스 소품은 뮤직 비디오 뿐만 아니라, 공연이나&nbsp;<br>인터뷰 촬영시에도 사용되었습니다.&nbsp;<br><br>헤드피스의 모델링 파일을 활용 하여 뮤지션의 라이브 방송과&nbsp;<br>팬들이 사용할 수 있는 인스타그램 AR 필터를 제작했습니다.<br><br>&nbsp;</p><p>Design &amp; Production</p><ul><li>Art Direction</li><li>Artwork Design</li><li>Art Prop Production</li><li>Instagram AR Filtter</li></ul>
     `,
     contents: "대충 내용",
     date: "2023-01-20",
@@ -539,18 +452,7 @@ const elements = [
     portfolioId: 9,
     title: "Crew 30 - THE FROST ON YOUR EDGE Aniversary MD",
     agency: "Dejavu Group",
-    introduction: `힙합 크루 Crew 30의 컴필레이션 앨범 [THE FROST ON YOUR EDGE] 의
-    음감회를 기념하여 머천다이즈 1종을 디자인 및 생산 하였습니다.
-    
-    해당 머천다이즈는 3월 6일 합정동 ‘데이토나 레코즈’ 에서 진행 되었던
-    음감회에서 판매 되었습니다.
-    
-    
-    
-    
-    
-    Design & Production
-    MD Production
+    introduction: `<p>힙합 크루 Crew 30의 컴필레이션 앨범 [THE FROST ON YOUR EDGE] 의<br>음감회를 기념하여 머천다이즈 1종을 디자인 및 생산 하였습니다.<br><br>해당 머천다이즈는 3월 6일 합정동 ‘데이토나 레코즈’ 에서 진행 되었던<br>음감회에서 판매 되었습니다.</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>Design &amp; Production</p><ul><li>MD Production</li></ul>
     `,
     contents: "대충 내용",
     date: "2022-10-31",
