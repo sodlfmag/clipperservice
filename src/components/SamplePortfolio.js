1a80a8ce0bb9afcb6ce1b719e54eac1041a4c311
sodlfmag <sodlfmag@khu.ac.kr>
2023-04-20 15:54:38 +0900

    STYLE: 포트폴리오 샘플 정보 입력

--- src/components/SamplePortfolio.js
+++ src/components/SamplePortfolio.js
@@ -1,5 +1,5 @@
 // const element = {
-//   src: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+//   src: process.env.PUBLIC_URL + `/assets/포트폴리오 사진//1.png`,
 //   name: "Owen - Cry",
 //   agency: "Eclipse Empire",
 //   date: "2022-10-31",
@@ -8,14 +8,51 @@
 const elements = [
   {
     portfolioId: 1,
-    title: "BIG naughty - Bucket List",
+    title: "BIG Naughty - 커피가게 아가씨(feat 원슈타인)",
     agency: "H1GHRMUSIC",
-    introduction: "대충 소개글",
+    introduction: `2월 2일 발매된 BIG Naughty님의 [커피가게 아가씨 Feat.원슈타인] 의
+    디지털 앨범 커버 디자인과 앨범커버에 사용된 소품을 제작했습니다.
+    앨범 커버에 사용된 소품은 곡의 비쥬얼 라이저 영상에도 활용되었습니다.
+    
+
+
+
+Design & Production
+Artwork Design
+Art Prop Production
+
+    `,
     contents: "대충 내용",
     date: "2022-10-31",
     clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/2.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/01_커피가게 아가씨/COF1.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/01_커피가게 아가씨/COF2.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/01_커피가게 아가씨/COF3.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/01_커피가게 아가씨/COF4.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/01_커피가게 아가씨/COF5.jpg`,
         thumbnail: false,
       },
     ],
@@ -23,84 +60,411 @@ const elements = [
 
   {
     portfolioId: 2,
-    title: "Clipper Service",
-    agency: "Clip",
-    introduction: "대충 소개글",
+    title: "BIG Naughty - Bucket List [EP]",
+    agency: "H1GERMUSIC’",
+    introduction: `2월 25일 발매된 BIG Naughty 님의 [Buket List]의
+    앨범 커버 디자인과, 피지컬 앨범 디자인을 진행 하였으며
+    앨범 커버에 사용된 소품을 제작하였습니다.
+    
+    해당 소품과 아트워크는 성수 미술관과 하이어뮤직의 협업으로
+    2월 24일부터 약 2개월 간 전시를 진행하였습니다.
+    
+    
+    Design & Production
+    Artwork Design
+    Physical Album Design
+    Art Prop Production
+    `,
     contents: "대충 내용",
-    date: "2020-11-12",
+    date: "2021-02-25",
     clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/3.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/02_서동현 버킷리스트/BK1.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/02_서동현 버킷리스트/BK2.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/02_서동현 버킷리스트/BK3.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/02_서동현 버킷리스트/BK4.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/02_서동현 버킷리스트/BK5.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/02_서동현 버킷리스트/BK6.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/02_서동현 버킷리스트/BK7.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/02_서동현 버킷리스트/BK8.jpg`,
         thumbnail: false,
       },
     ],
   },
   {
     portfolioId: 3,
-    title: "Owen - Cry",
-    agency: "Eclipse Empire",
-    introduction: "대충 소개글",
+    title: "나선 - Underdogma",
+    agency: "PlusN Ent",
+    introduction: ` 8월 24일 발매된 나선님의 Underdogma[EP] 의
+    디지털 앨범 커버의 디자인과, 앨범 커버에 사용된 소품을 제작했습니다.
+    
+    
+    
+    
+    
+    
+    Design & Production
+    Artwork Design
+    Art Prop
+    `,
     contents: "대충 내용",
-    date: "2022-10-31",
+    date: "2021-08-24",
     clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/03_나선 Underdogma/NUD1.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/03_나선 Underdogma/NUD2.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/03_나선 Underdogma/NUD3.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/03_나선 Underdogma/NUD4.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/03_나선 Underdogma/NUD5.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/03_나선 Underdogma/NUD6.jpg`,
         thumbnail: false,
       },
     ],
   },
   {
     portfolioId: 4,
-    title: "MARV - MAMA",
-    agency: "Eclipse Empire",
-    introduction: "대충 소개글",
+    title: "BIG Naughty - STAB (Feat. eaJ) (Prod. dress)",
+    agency: "H1GERMUSIC",
+    introduction: `11월 18일 발매된 BIG Naughty님의 [STAB] 의
+    디지털 앨범 커버의 디자인과 앨범 커버에 사용된 소품을 제작했습니다.
+    앨범 커버에 사용된 소품은 곡의 비쥬얼 라이저 영상에도 활용되었습니다.
+    
+    
+    
+    
+    
+    Design & Production
+    Artwork Design
+    Art Prop Production
+    Visualizer video
+    `,
     contents: "대충 내용",
     date: "2022-12-20",
     clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/5.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/04_서동현 STAB/STAB1.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/04_서동현 STAB/STAB2.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/04_서동현 STAB/STAB3.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/04_서동현 STAB/STAB4.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/04_서동현 STAB/STAB5.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/04_서동현 STAB/STAB6.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/04_서동현 STAB/STAB7.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/04_서동현 STAB/STAB8.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/04_서동현 STAB/STAB9.jpg`,
         thumbnail: false,
       },
     ],
   },
   {
     portfolioId: 5,
-    title: "Khundipanda - The Spoild child 1st Aniversary MD",
-    agency: "Dejavu group",
-    introduction: "대충 소개글",
+    title: "Owen - Cry",
+    agency: "Eclipse Empire’",
+    introduction: `10월 31일 발매된 Owen님의 정규 앨범 [CRY] 의
+    디지털 앨범 커버와, 타이틀 곡 Jungle 에 사용된
+    뮤직비디오/공연용 헤드피스 소품을 제작했습니다.
+    
+    헤드피스의 모델링 파일을 활용 하여 뮤지션의 라이브 방송과
+    팬들이 사용할 수 있는 인스타그램 AR 필터를 제작했습니다.
+    
+    
+    
+    
+    
+    
+    Design & Production
+    Art Prop (Headpiece)
+    Instagram AR Filtter
+    `,
     contents: "대충 내용",
-    date: "2023-01-20",
+    date: "2022-10-31",
     clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/4.png`,
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/05_CRY/CRY1.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/05_CRY/CRY2.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/05_CRY/CRY3.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/05_CRY/CRY4.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/05_CRY/CRY5.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/05_CRY/CRY6.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/05_CRY/CRY7.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/05_CRY/CRY8.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/05_CRY/CRY9.jpg`,
         thumbnail: false,
       },
     ],
   },
   {
     portfolioId: 6,
-    title: "MARV - SOULY",
+    title: "Marv - MAMA [pre-release]",
     agency: "Eclipse Empire",
-    introduction: "대충 소개글",
+    introduction: `12월 20일 발매된 Marv님의  [MAMA]
+    디지털 앨범 커버의 디자인과, Marv님의 음악적 정체성을 담은
+    뮤직비디오/촬영용 헤드피스 소품을 제작했습니다.
+    
+    헤드피스의 모델링 파일을 활용 하여 뮤지션의 라이브 방송과
+    팬들이 사용할 수 있는 인스타그램 AR 필터를 제작했습니다.
+    
+    
+    
+    
+    Design & Production
+    Artwork Design
+    Art Prop (Headpiece)
+    Instagram AR Filtter
+    `,
     contents: "대충 내용",
-    date: "2023-01-20",
+    date: "2022-12-20",
     clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/6.png`,
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/06_MAMA/MAMA1.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/06_MAMA/MAMA2.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/06_MAMA/MAMA3.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/06_MAMA/MAMA4.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/06_MAMA/MAMA5.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL + `/assets/포트폴리오 사진/06_MAMA/MAMA6.jpg`,
         thumbnail: false,
       },
     ],
   },
   {
     portfolioId: 7,
-    title: "BIG naughty - Bucket List",
-    agency: "H1GHRMUSIC",
-    introduction: "대충 소개글",
+    title: "Khundipanda - The Spoild child 1st Aniversary MD",
+    agency: "Dejavu Group",
+    introduction: `Khundipanda 님의 정규 앨범 [The Spoild Child:균] 의 발매 1주년을
+    기념하여 머천다이즈 3종을(뱃지, 쿠션, 후드) 디자인 및 생산했습니다.
+    
+    해당 머천다이즈 중 2종 (뱃지, 쿠션)은 데자부 스토어에서 선 공개되어 판매 되었고
+    후드는 1월 17일에 왓차홀에서 진행 되었던 단독 콘서트에서 판매 되었습니다.
+    
+    머천다이즈의 디자인과 생산 뿐만 아니라 해당 단독 콘서트에서
+    사용된 무대 소품또한 제작 하였습니다.
+    
+    
+    
+    Design & Production
+    MD Production
+    Stage Props Production
+    `,
     contents: "대충 내용",
-    date: "2022-10-31",
+    date: "2023-01-17",
     clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/2.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM1.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM2.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM3.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM4.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM5.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM6.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM7.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM8.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM9.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/07_쿤디판다 MD/KM10.jpg`,
         thumbnail: false,
       },
     ],
@@ -108,70 +472,126 @@ const elements = [
 
   {
     portfolioId: 8,
-    title: "Clipper Service",
-    agency: "Clip",
-    introduction: "대충 소개글",
+    title: "Marv - Souly [EP]",
+    agency: "Eclipse Empire",
+    introduction: `1월 20일 발매된 Marv님의 EP 앨범 [SOULY] 의
+    앨범 전반적인 아트 디렉팅과 디지털 앨범 커버 디자인을 맡았습니다.
+    
+    또한 Marv님의 음악적 정체성을 담은 뮤직비디오/촬영용 헤드피스 소품을
+    제작했습니다. 헤드피스 소품은 뮤직 비디오 뿐만 아니라, 공연이나
+    인터뷰 촬영시에도 사용되었습니다.
+    
+    헤드피스의 모델링 파일을 활용 하여 뮤지션의 라이브 방송과
+    팬들이 사용할 수 있는 인스타그램 AR 필터를 제작했습니다.
+    
+    
+    Design & Production
+    Art Direction
+    Artwork Design
+    Art Prop Production
+    Instagram AR Filtter
+    `,
     contents: "대충 내용",
-    date: "2020-11-12",
+    date: "2023-01-20",
     clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/3.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/08_SOULY/SOULY1.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/08_SOULY/SOULY2.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/08_SOULY/SOULY3.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/08_SOULY/SOULY4.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/08_SOULY/SOULY5.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/08_SOULY/SOULY6.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/08_SOULY/SOULY7.jpg`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/08_SOULY/SOULY8.jpg`,
         thumbnail: false,
       },
     ],
   },
   {
     portfolioId: 9,
-    title: "Owen - Cry",
-    agency: "Eclipse Empire",
-    introduction: "대충 소개글",
+    title: "Crew 30 - THE FROST ON YOUR EDGE Aniversary MD",
+    agency: "Dejavu Group",
+    introduction: `힙합 크루 Crew 30의 컴필레이션 앨범 [THE FROST ON YOUR EDGE] 의
+    음감회를 기념하여 머천다이즈 1종을 디자인 및 생산 하였습니다.
+    
+    해당 머천다이즈는 3월 6일 합정동 ‘데이토나 레코즈’ 에서 진행 되었던
+    음감회에서 판매 되었습니다.
+    
+    
+    
+    
+    
+    Design & Production
+    MD Production
+    `,
     contents: "대충 내용",
     date: "2022-10-31",
     clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/1.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/09_30 크루 컴필/3CM1.png`,
         thumbnail: false,
       },
-    ],
-  },
-  {
-    portfolioId: 10,
-    title: "MARV - MAMA",
-    agency: "Eclipse Empire",
-    introduction: "대충 소개글",
-    contents: "대충 내용",
-    date: "2022-12-20",
-    clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/5.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/09_30 크루 컴필/3CM2.png`,
         thumbnail: false,
       },
-    ],
-  },
-  {
-    portfolioId: 11,
-    title: "Khundipanda - The Spoild child 1st Aniversary MD",
-    agency: "Dejavu group",
-    introduction: "대충 소개글",
-    contents: "대충 내용",
-    date: "2023-01-20",
-    clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/4.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/09_30 크루 컴필/3CM3.png`,
         thumbnail: false,
       },
-    ],
-  },
-  {
-    portfolioId: 12,
-    title: "MARV - SOULY",
-    agency: "Eclipse Empire",
-    introduction: "대충 소개글",
-    contents: "대충 내용",
-    date: "2023-01-20",
-    clipperImageDtos: [
       {
-        fileName: process.env.PUBLIC_URL + `/assets/portfolios/6.png`,
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/09_30 크루 컴필/3CM4.png`,
+        thumbnail: false,
+      },
+      {
+        fileName:
+          process.env.PUBLIC_URL +
+          `/assets/포트폴리오 사진/09_30 크루 컴필/3CM5.png`,
         thumbnail: false,
       },
     ],
