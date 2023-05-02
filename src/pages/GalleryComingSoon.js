732652cfe504eb17afab51bddb333bd0a4a42144
sodlfmag <sodlfmag@khu.ac.kr>
2023-05-02 16:17:31 +0900

    FIX: 갤러리 슬라이드 컴포넌트 제거 후 커밍순 이미지 삽입

---
+++ src/pages/GalleryComingSoon.js
@@ -0,0 +1,28 @@
+import ImageSlide from "../components/ImageSlide";
+import ImageSlide4 from "../components/ImageSlide copy 2";
+import MyFooter from "../components/MyFooter";
+import PageBody from "../components/PageBody";
+import { ProgressBars } from "../components/ProgressBar";
+
+const GalleryComingSoon = () => {
+  return (
+    <>
+      <PageBody
+        name="GALLERY"
+        introduction="클리퍼의 단독 프로젝트를 매거진 형태로 만나보세요."
+      />
+      <div>
+        <div className="GalleryTitle">Clipper Collection</div>
+        <ImageSlide />
+      </div>
+      <div>
+        <div className="GalleryTitle">Clipper Service Personal work</div>
+        <ImageSlide4 />
+      </div>
+      <div style={{ marginBottom: "150px" }}></div>
+      <MyFooter />
+    </>
+  );
+};
+
+export default GalleryComingSoon;
